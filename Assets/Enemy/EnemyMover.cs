e64dd1e5b922172a6653279c711ebf2e61b0112f
Timothy Truty <ttruty@gmail.com>
2023-01-17 06:30:07 -0600

    collider to tile

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -6,20 +6,30 @@ public class EnemyMover : MonoBehaviour
 {
     [SerializeField] private List<Waypoint> path = new List<Waypoint>();
 
-    [SerializeField] private float waitTime = 1f;
+    [SerializeField] [Range(0f, 5f)] private float speed = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        PrintWaypointName();
-        StartCoroutine(PrintWaypointName());
+        StartCoroutine(FollowPath());
     }
 
-    IEnumerator PrintWaypointName()
+    IEnumerator FollowPath()
     {
         foreach (Waypoint waypoint in path)
         { 
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            while (travelPercent < 1f) {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+
+
         }
     }
 }
--- Assets/Tiles/Waypoint.cs
+++ Assets/Tiles/Waypoint.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    private void OnMouseOver() {
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("");
     }
 }
